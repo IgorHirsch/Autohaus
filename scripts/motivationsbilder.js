50f63be31898a23e0f9db2530d5856deb65bec51
igor hirsch <g1rsh.egor@gmail.com>
2024-12-11 20:16:07 +0100

    CarouselWiev Motivation Test

--- scripts/motivationsbilder.js
+++ scripts/motivationsbilder.js
@@ -1,8 +1,13 @@
 const swiper = new Swiper(".swiper-container", {
-  loop: false, // Endlose Schleife
-  slidesPerView: 1, // Nur ein Slide gleichzeitig anzeigen
+  loop: true, // Endlosschleife
+  slidesPerView: 1, // Nur ein Bild gleichzeitig anzeigen
+  spaceBetween: 10, // Abstand zwischen den Bildern (optional)
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: ".swiper-pagination", // Pagination hinzufügen
+    clickable: true, // Ermöglicht das Klicken auf die Seitenzahlen
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", // Weiter-Button
+    prevEl: ".swiper-button-prev", // Zurück-Button
   },
 });
