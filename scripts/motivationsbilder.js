2bafb423a4fcabef14f1191a7a5d8f11a0021a1a
igor hirsch <g1rsh.egor@gmail.com>
2024-12-11 20:29:44 +0100

    Carousel 3

--- scripts/motivationsbilder.js
+++ scripts/motivationsbilder.js
@@ -11,3 +11,22 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev", // Zurück-Button
   },
 });
+
+
+window.addEventListener("resize", () => {
+  const swiperContainer = document.querySelector(".swiper-container");
+  const imagesCard = document.querySelector(".motivation__images-card");
+
+  if (window.innerWidth <= 768) {
+    // Bei kleineren Bildschirmen nur Swiper anzeigen
+    swiperContainer.style.display = "block";
+    imagesCard.style.display = "none";
+  } else {
+    // Bei größeren Bildschirmen die Bilder nebeneinander anzeigen
+    swiperContainer.style.display = "none";
+    imagesCard.style.display = "flex";
+  }
+});
+
+// Initiale Anzeige bei Seitenladen
+window.dispatchEvent(new Event("resize"));
