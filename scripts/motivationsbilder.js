754b71e98cf2114920f62ba3426423eb93223187
igor hirsch <g1rsh.egor@gmail.com>
2024-12-12 19:17:05 +0100

    carousel bearbeitet

--- scripts/motivationsbilder.js
+++ scripts/motivationsbilder.js
@@ -1,5 +1,5 @@
 const swiper = new Swiper(".swiper-container", {
-  loop: false, // Endlosschleife
+  loop: true, // Endlosschleife
   slidesPerView: 1, // Abstand zwischen den Bildern (optional)
   pagination: {
     el: ".swiper-pagination", // Pagination hinzufügen
@@ -11,16 +11,16 @@ const swiper = new Swiper(".swiper-container", {
   },
 });
 
-
 window.addEventListener("resize", () => {
   const swiperContainer = document.querySelector(".swiper-container");
+
   const imagesCard = document.querySelector(".motivation__images-card");
 
   if (window.innerWidth <= 768) {
     // Bei kleineren Bildschirmen nur Swiper anzeigen
     swiperContainer.style.display = "block";
     imagesCard.style.display = "none";
-    
+
     nav.classList.remove("navigation");
   } else {
     // Bei größeren Bildschirmen die Bilder nebeneinander anzeigen
